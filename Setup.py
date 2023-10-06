6d4aefc239c55a6c2a393624b43d3447a9bf8022
F-Fawzi <f.fawzi@enim.ca>
2023-10-05 22:05:04 -0400

    modified setup

--- Setup.py
+++ Setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import find_packages, setup
 
 setup(
@@ -9,16 +8,4 @@ setup(
     description='Sample project repo for IFT6758-2021',
     author='[FAWZI FATIMA ZAHRA, Frida-Cecilia, Franck Sharif, Francis]',
     license='',
-=======
-from setuptools import find_packages, setup
-
-setup(
-    name='ift6758',
-    packages=find_packages(),
-    # package_dir={'': 'src'},
-    version='0.1.0',
-    description='Sample project repo for IFT6758-2021',
-    author='[FAWZI FATIMA ZAHRA, Frida-Cecilia, Franck Sharif, Francis]',
-    license='',
->>>>>>> 7ec857dbd7fa24b4dcff7b832d69204db1484172
 )
